629cf862d0739c76e0170f04bf4ab56d3b670756
Junio Sousa <juniosousa120@gmail.com>
2022-10-26 05:53:50 -0300

    Acordion e navtab js

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
-const test = document.querySelector("h1");
+// Navegação por tabs
 
-test.addEventListener("click", function () {
-  console.log("teste ok");
-});
+function initNavTab() {
+  const tabMenu = document.querySelectorAll(".js-tabMenu img");
+  const tabContent = document.querySelectorAll(".js-conteudo .info");
+
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("ativo");
+
+    function ativeTab(index) {
+      tabContent.forEach((info) => {
+        info.classList.remove("ativo");
+      });
+
+      tabContent[index].classList.add("ativo");
+    }
+
+    tabMenu.forEach((img, nu) => {
+      img.addEventListener("click", () => {
+        ativeTab(nu);
+      });
+    });
+  }
+}
+
+initNavTab();
+
+function initAcordion() {
+  const acordionList = document.querySelectorAll(".js-acordion dt");
+
+  if (acordionList.length) {
+    acordionList[0].classList.add("ativo");
+    acordionList[0].nextElementSibling.classList.add("ativo");
+
+    function activeAcordion() {
+      this.classList.toggle("ativo");
+      this.nextElementSibling.classList.toggle("ativo");
+    }
+
+    acordionList.forEach((item) => {
+      item.addEventListener("click", activeAcordion);
+    });
+  }
+}
