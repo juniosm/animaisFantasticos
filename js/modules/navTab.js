82f4205886e57f018c276d05ade5940675b06efa
Junio Sousa <juniosousa120@gmail.com>
2023-02-01 05:12:55 -0300

    NavTab em class

--- js/modules/navTab.js
+++ js/modules/navTab.js
@@ -1,24 +1,31 @@
 // Navegação por tabs
 
-export default function initNavTab() {
-  const tabMenu = document.querySelectorAll("[data-tab='menu'] img");
-  const tabContent = document.querySelectorAll("[data-tab='conteudo'] .info");
-
-  function ativeTab(index) {
-    tabContent.forEach(info => {
-      info.classList.remove("ativo");
-    });
-    const direcao = tabContent[index].dataset.anime;
-    tabContent[index].classList.add("ativo", direcao);
+export default class NavTab {
+  constructor(listMenu, listContent) {
+    this.tabMenu = document.querySelectorAll(listMenu);
+    this.tabContent = document.querySelectorAll(listContent);
   }
 
-  if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add("ativo");
+  ativeTab(index) {
+    this.tabContent.forEach(info => {
+      info.classList.remove("ativo");
+    });
+    const direcao = this.tabContent[index].dataset.anime;
+    this.tabContent[index].classList.add("ativo", direcao);
+  }
 
-    tabMenu.forEach((img, nu) => {
+  addEventNavTab() {
+    this.tabMenu.forEach((img, nu) => {
       img.addEventListener("click", () => {
-        ativeTab(nu);
+        this.ativeTab(nu);
       });
     });
+  }
+
+  init() {
+    if (this.tabMenu.length && this.tabContent.length) {
+      this.ativeTab(0);
+      this.addEventNavTab();
+    }
   }
 }
